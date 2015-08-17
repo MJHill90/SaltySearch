07375ee19d58d01094b8de5c1c012345aaab9402
MJHill90 <matthew.james.hill90@gmail.com>
2015-08-17 20:13:50 +0100

    Initial project setup

---
+++ SaltySearch/SaltySearch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SaltySearch.Startup))]
+namespace SaltySearch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
